e3a4e65b6abac083aa96747a91a65dedc2ee5a3b
p.p <554096147@qq.com>
2021-05-21 10:59:44 +0800

    feat: 修改bug

--- src/components/map/service/utils.js
+++ src/components/map/service/utils.js
@@ -59,3 +59,28 @@ export function deepClone(target) {
   }
   return _deepCopy(target);
 }
+
+export function getRatio()
+{
+    var ratio=0;
+    var screen=window.screen;
+    var ua=navigator.userAgent.toLowerCase();
+
+    if(window.devicePixelRatio !== undefined)
+    {
+        ratio=window.devicePixelRatio;    
+    }
+    else if(~ua.indexOf('msie'))
+    {
+        if(screen.deviceXDPI && screen.logicalXDPI)
+        {
+            ratio=screen.deviceXDPI/screen.logicalXDPI;        
+        }
+    
+    }
+    else if(window.outerWidth !== undefined && window.innerWidth !== undefined)
+    {
+        ratio=window.outerWidth/window.innerWidth;
+    }
+    return ratio;
+}
